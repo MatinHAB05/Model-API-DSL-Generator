a26eb682d1d30fc5dd567a2a1f91dfe034c07859
Matin Baki <m9652973@gmail.com>
2026-01-25 01:36:52 +0330

    ready for debuging

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-from antlr4 import TokenStreamRewriter , FileStream , CommonTokenStream , ParseTreeWalker , InputStream
+from antlr4 import TokenStreamRewriter , FileStream , CommonTokenStream , ParseTreeWalker , InputStream ,
 from CustomListner_ast_tree import AST_Listener
 
+from gen import backendgrammerLexer , backendgrammerParser
+# DEBUGING MODE !!!
+
+stream = FileStream(r"test_grammer_files\test_debugingMode.txt")
+lexer = backendgrammerLexer(stream)
+token_stream = CommonTokenStream(lexer)
+parser = backendgrammerParser(token_stream)
+parse_tree = parser.compileinit()
+
+# --- Build AST with CustomListener ---
+ast_builder_listener = AST_Listener()
+walker = ParseTreeWalker()
+walker.walk(t=parse_tree, listener=ast_builder_listener)
+ast = ast_builder_listener.ast
